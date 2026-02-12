26dbf4cf3a8225a803e0376523250ae62218e471
Andrei Constantin Alexandru <programmer4web@users.noreply.github.com>
2026-02-12 15:50:07 +0200

    Dynamic routes for fruits

---
+++ app/fruits/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import fruits from '@/data/fruits.json';
+
+const FruitDetailPage = async ({ params } : { params: Promise<{ id: string }> }) => {
+    const { id } = await params;
+
+    const fruit = fruits.find(f => f.id === id);
+    if (!fruit) {
+        notFound();
+    }
+
+    return <div className="w-full max-w-2xl mx-auto p-8">
+        <h1>{fruit.label}</h1>
+        <p>{fruit.category}</p>
+    </div>
+}
+
+export default FruitDetailPage;
